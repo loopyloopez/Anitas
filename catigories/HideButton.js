18963e5a18f37d577ed023f24d78fb906283266d
loopyloopez <pin.filipe.l@gmail.com>
2025-05-21 14:25:09 -0500

    showing users food is lunch only

--- catigories/HideButton.js
+++ catigories/HideButton.js
@@ -1,5 +1,5 @@
 const buttons = document.getElementsByClassName('exclusive');
-
+const blockes = document.getElementsByClassName('blocked');
 const now = new Date();
 const time = now.getHours();
 const day = now.getDay();
@@ -10,11 +10,25 @@ if(day<=5){
     if(time < 11 || time > 14){
         for(i=0;i<buttons.length;i++){
             buttons[i].style.display="none"
+            
         }
-    
-        
-
+        enableblockedbutton();
+    }
+    else{
+        disableblockbutton();
     }
    
-
+}
+else{
+    disableblockbutton();
+}
+function enableblockedbutton(){
+    for(x=0;x<=blockes.length;x++){
+        blockes[x].style.display='block';
+    }
+}
+function disableblockbutton(){
+    for(x=0;x<=blockes.length;x++){
+        blockes[x].style.display='none';
+    }
 }
